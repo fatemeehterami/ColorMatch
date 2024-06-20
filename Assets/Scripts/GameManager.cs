797930ea7b6505f38fa3f77f99775d89c54bcfc5
fateme ehterami <fateme.ehterami8214@gmail.com>
2024-06-20 08:39:51 +0330

    continue gameManager code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,37 +1,77 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
+using System.Collections.Generic;
+
 
 public class GameManager : MonoBehaviour
 {
     public Image[] images;
     int[] arrayOfNums = {0,1,2,3,4,5,6,7};
-    // Start is called before the first frame update
+    Dictionary<string,Color> colors;
+    public Color colorToPick;
+    public int score;
+    public Text pickTxt;
+    public Text scoreTxt;
+
     void Start()
     {
+        colors = new Dictionary<string, Color>
+        {
+            { "blue", Color.blue },
+            { "cyan", Color.cyan },
+            { "gray", Color.gray },
+            { "green", Color.green },
+            { "magenta", Color.magenta },
+            { "red", Color.red },
+            { "white", Color.white },
+            { "yellow", Color.yellow }
+        };
         images = GetComponentsInChildren<Image>();
+        setupcolor();
+        setupText();
     }
-
-    // Update is called once per frame
     void Update()
     {
         
+    }
+    public void setupcolor(){
+        images = GetComponentsInChildren<Image>();
+        arrayOfNums = arrayOfNums.OrderBy(i=>Random.Range(0,images.Length)).ToArray();
+        int newNum = 0 ;
+        foreach(Image img in images){
+            img.color = setColor(arrayOfNums[newNum]);
+            newNum++;
+        }
+
+    }
+    public void setupText(){
+        int rand = Random.Range(0,colors.Count);
+        pickTxt.text = colors.ElementAt(rand).Key;
+        colorToPick = colors.ElementAt(rand).Value;
+        pickTxt.color = setColor(Random.Range(0,7));
+        scoreTxt.text = "Score: "+ score;
     }
     public Color setColor(int numInArray){
         switch(numInArray){
             case 0:
                 return Color.blue;
             case 1:
-                return Color.blue;
+                return Color.cyan;
             case 2:
-                return Color.blue;
+                return Color.gray;
             case 3:
-                return Color.blue;
+                return Color.green;
             case 4:
-                return Color.blue;
+                return Color.magenta;
             case 5:
-                return Color.blue;
+                return Color.red;
+            case 6:
+                return Color.white;
+            case 7:
+                return Color.yellow;
+            default:
+                return Color.clear;
 
         }
     }
