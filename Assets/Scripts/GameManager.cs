57c0f9b2e95133b9c9929998ee4c1eff52480ec8
fateme ehterami <fateme.ehterami8214@gmail.com>
2024-06-20 18:47:19 +0330

    add gameStartBg, gameOverBg , sounds

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 
 public class GameManager : MonoBehaviour
@@ -11,12 +12,18 @@ public class GameManager : MonoBehaviour
     Dictionary<string, Color> colors;
     public Color colorToPick;
     public int score;
-    public int heart = 3;
+    public int heart;
     public Text pickTxt;
     public Text scoreTxt;
     public Text heartTxt;
     public Button resetBtn;
+    public Button startBtn;
     public GameObject gameOverBg;
+    public GameObject startBg;
+    public AudioClip correctSound;
+    public AudioClip wrongSound;
+    public AudioClip gameoverSound;
+    private AudioSource audioSource;
     void Start()
     {
         colors = new Dictionary<string, Color>
@@ -31,17 +38,10 @@ public class GameManager : MonoBehaviour
             { "yellow", Color.yellow }
         };
         images = GetComponentsInChildren<Image>();
-
+        audioSource = GetComponent<AudioSource>();
         resetBtn.onClick.AddListener(ResetGame);
-
-        if (gameOverBg != null)
-        {
-            gameOverBg.SetActive(false);
-        }
-        else
-        {
-            Debug.LogError("Game Over Background is not assigned!");
-        }
+        startBtn.onClick.AddListener(StartGame);
+        heart = 3;
         setupcolor();
         setupText();
     }
@@ -99,15 +99,21 @@ public class GameManager : MonoBehaviour
             setupText();
             score++;
             scoreTxt.text = "Score: " + score;
+            PlaySound(correctSound);
         }
         else
         {
-            score--;
             heart--;
-            heartTxt.text = "Live: " + heart;
+            score--;
+            PlaySound(wrongSound);
             if (heart == 0)
             {
                 GameOver();
+                PlaySound(gameoverSound);
+            }
+            else
+            {
+                heartTxt.text = "Live: " + heart;
             }
         }
     }
@@ -118,13 +124,26 @@ public class GameManager : MonoBehaviour
     }
     void ResetGame()
     {
-
         gameOverBg.SetActive(false);
-
+        startBg.SetActive(true);
         heart = 3;
         score = 0;
         scoreTxt.text = "Score: " + score;
         setupcolor();
         setupText();
     }
+    void PlaySound(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+    void StartGame()
+    {
+        startBg.SetActive(false);
+        heart = 3;
+        score = 0;
+        setupcolor();
+        setupText();
+    }
+
 }
