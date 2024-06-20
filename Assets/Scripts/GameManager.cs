36d6225b91b6cbb8738d9c79d0a2cf7e09838f2b
fateme ehterami <fateme.ehterami8214@gmail.com>
2024-06-20 09:38:03 +0330

    complete game and add image for gameover

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,13 +7,16 @@ using System.Collections.Generic;
 public class GameManager : MonoBehaviour
 {
     public Image[] images;
-    int[] arrayOfNums = {0,1,2,3,4,5,6,7};
-    Dictionary<string,Color> colors;
+    int[] arrayOfNums = { 0, 1, 2, 3, 4, 5, 6, 7 };
+    Dictionary<string, Color> colors;
     public Color colorToPick;
     public int score;
+    public int heart = 3;
     public Text pickTxt;
     public Text scoreTxt;
-
+    public Text heartTxt;
+    public Button resetBtn;
+    public GameObject gameOverBg;
     void Start()
     {
         colors = new Dictionary<string, Color>
@@ -28,32 +31,45 @@ public class GameManager : MonoBehaviour
             { "yellow", Color.yellow }
         };
         images = GetComponentsInChildren<Image>();
+
+        resetBtn.onClick.AddListener(ResetGame);
+
+        if (gameOverBg != null)
+        {
+            gameOverBg.SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("Game Over Background is not assigned!");
+        }
         setupcolor();
         setupText();
     }
-    void Update()
+    public void setupcolor()
     {
-        
-    }
-    public void setupcolor(){
         images = GetComponentsInChildren<Image>();
-        arrayOfNums = arrayOfNums.OrderBy(i=>Random.Range(0,images.Length)).ToArray();
-        int newNum = 0 ;
-        foreach(Image img in images){
+        arrayOfNums = arrayOfNums.OrderBy(i => Random.Range(0, images.Length)).ToArray();
+        int newNum = 0;
+        foreach (Image img in images)
+        {
             img.color = setColor(arrayOfNums[newNum]);
             newNum++;
         }
 
     }
-    public void setupText(){
-        int rand = Random.Range(0,colors.Count);
-        pickTxt.text = colors.ElementAt(rand).Key;
+    public void setupText()
+    {
+        int rand = Random.Range(0, colors.Count);
+        pickTxt.text = "Click " + colors.ElementAt(rand).Key;
         colorToPick = colors.ElementAt(rand).Value;
-        pickTxt.color = setColor(Random.Range(0,7));
-        scoreTxt.text = "Score: "+ score;
+        pickTxt.color = setColor(Random.Range(0, 7));
+        scoreTxt.text = "Score: " + score;
+        heartTxt.text = "Live: " + heart;
     }
-    public Color setColor(int numInArray){
-        switch(numInArray){
+    public Color setColor(int numInArray)
+    {
+        switch (numInArray)
+        {
             case 0:
                 return Color.blue;
             case 1:
@@ -74,5 +90,41 @@ public class GameManager : MonoBehaviour
                 return Color.clear;
 
         }
+    }
+    public void checkColor(Image image)
+    {
+        if (image.color == colorToPick)
+        {
+            setupcolor();
+            setupText();
+            score++;
+            scoreTxt.text = "Score: " + score;
+        }
+        else
+        {
+            score--;
+            heart--;
+            heartTxt.text = "Live: " + heart;
+            if (heart == 0)
+            {
+                GameOver();
+            }
+        }
+    }
+    void GameOver()
+    {
+        gameOverBg.SetActive(true);
+
+    }
+    void ResetGame()
+    {
+
+        gameOverBg.SetActive(false);
+
+        heart = 3;
+        score = 0;
+        scoreTxt.text = "Score: " + score;
+        setupcolor();
+        setupText();
     }
 }
